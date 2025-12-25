ae331bad3a63ec190ebbdfcbcbbb0a22e6fde76f
shree <shreemahor5@gmail.com>
2025-12-25 09:44:46 -0700

    Fantasy Storytelling

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from typing import TypedDict, Annotated
 from langgraph.graph import add_messages, StateGraph, END
 from langchain_core.messages import AIMessage, HumanMessage
 from langgraph.checkpoint.memory import MemorySaver
-from langchain_core import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
+
 
 openrouter_api_key = os.getenv("OPENROUTER_API_KEY")
-
-llm = ChatOpenAI(model="openrouter/auto",
+llm = ChatOpenAI(model="allenai/olmo-3.1-32b-think:free",
                 api_key=openrouter_api_key,
                 base_url="https://openrouter.ai/api/v1"
 )
@@ -16,27 +16,36 @@ llm = ChatOpenAI(model="openrouter/auto",
 memory = MemorySaver()
 
 prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful pirate. Chat with the user friendly but always respond like a pirate."),
+    ("system", "You are a fantasy game dungeon master. You will guide the user through an interactive story where they make choices that affect the outcome of the adventure." \
+    "Describe scenes vividly and present the user with clear choices at each step. Keep the story engaging and immersive."),
     ("human", "{text}")
 ])
 chain = prompt | llm
 
+initial_situation = llm.invoke("You are a fantasy storyteller. Create an interesting and engaging opening situation for a new story." \
+"It should force the user to make a choice in the start of their adventure.").content
+print(initial_situation)
+
 class BasicChatState(TypedDict): 
     messages: Annotated[list, add_messages]
+    situation: str
 
 
 def storyteller(state: BasicChatState): 
     return {
-       "messages": [llm.invoke(state["messages"])]
+       "messages": [chain.invoke(state["messages"])]
     }
 
+
 graph = StateGraph(BasicChatState)
+
+graph.set_entry_point("storyteller")
+
+#graph.add_node("situation")
 
 graph.add_node("storyteller", storyteller)
 
 graph.add_edge("storyteller", END)
-
-graph.set_entry_point("storyteller")
 
 app = graph.compile(checkpointer=memory)
 print(app.get_graph().draw_ascii())
@@ -45,13 +54,14 @@ config = {"configurable": {
     "thread_id": 1
 }}
 
-while True: 
-    user_input = input("You: ")
-    if(user_input in ["exit", "end"]):
-        break
-    else: 
-        result = app.invoke({
-            "messages": [HumanMessage(content=user_input)]
-        }, config=config)
+if __name__ == "__main__": 
+    while True: 
+        user_input = input("You: ")
+        if(user_input in ["exit", "end", "quit", "bye"]):
+            break
+        else: 
+            result = app.invoke({
+                "messages": [HumanMessage(content=user_input)]
+            }, config=config)
 
-        print("Fable Friend: " + result["messages"][-1].content)
+            print("Fable Friend: " + result["messages"][-1].content)
---
+++ testing.py
@@ -0,0 +1,3 @@
+from main import llm, initial_situation
+
+print(initial_situation.content)
